11cab01e575053b9715bce86de07253327976515
yeon2731 <yeon2731@gmail.com>
2025-08-01 01:03:45 +0900

    learning vite

---
+++ week4/react-for-beginners/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import styles from "./App.module.css";
+import Button from "./Button.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1 className={styles.title}>Welcome back!</h1>
+      <Button text={"Continue"} />
+    </div>
+  );
+}
+
+export default App;
