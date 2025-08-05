d4a7733c2db2c7ffbcb73a0c76bd36c4da8c6a7d
yeon2731 <yeon2731@gmail.com>
2025-08-05 20:04:40 +0900

    0805

--- week4/react-for-beginners/src/App.jsx
+++ week4/react-for-beginners/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import styles from "./App.module.css";
 import Button from "./Button.jsx";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created");
+    return () => console.log("destroyed");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
